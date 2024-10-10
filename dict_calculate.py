85459f71db73bd6e49006b633a72d768cfd84e0e
ms0626 <marius.sutkus@ext.fmc-ag.com>
2024-10-10 13:52:55 +0300

    -**

--- dict_calculate.py
+++ dict_calculate.py
@@ -6,12 +6,30 @@ from pathlib import Path
 
 
 def avg(par: float):
-    return round(sum(par)/len(par),2)
+    return sum(par)/len(par)
 
 def print_data(name_val: dict):
 
     for name, val in sorted(name_val.items()):
         print(f'{name}: {[min(val), avg(val), max(val)]}')
+
+
+def print_data_01(name_val: dict):
+
+
+    for name, val in sorted(name_val.items()):
+        val_f = [float(elm) for elm in val]
+        print(f'{name}: {[min(val_f), avg(val_f), max(val_f)]}')
+
+
+def print_data_02(name_val: dict):
+    min_v = 'min_v'
+    max_v = 'max_v'
+    sum_v = 'sum_v'
+    count = 'count'
+    for name, val in sorted(name_val.items()):
+        print(f'{name}: min: {val[min_v]}, max: {val[max_v]}, avg: {val[sum_v]/val[count]}')
+
 
 def logging_data(name_val: dict):
     logging.basicConfig(format='%(message)s', level=logging.DEBUG)
@@ -99,17 +117,56 @@ def read_file_5(path_to_file: Path):
     return name_val
 
 
+def read_file_6(path_to_file: Path):
+    name_val = {}
+
+    with path_to_file.open() as file:
+        for line in file:
+            name, val = line.split(';')
+            if name_val.get(name) is None:
+                name_val[name] = [val]
+            else:
+                name_val[name].append(val)
+
+def read_file_7(path_to_file: Path):
+    min_v = 'min_v'
+    max_v = 'max_v'
+    sum_v = 'sum_v'
+    count = 'count'
+    name_val = {}
+
+    with path_to_file.open() as file:
+        for line in file:
+            name, val = line.split(';')
+            val = float(val)
+
+            if name_val.get(name) is None:
+                name_val[name] = {}
+                name_val[name][min_v] = val
+                name_val[name][max_v] = val
+                name_val[name][sum_v] = val
+                name_val[name][count] = 1
+
+            else:
+                name_val[name][count] += 1
+                name_val[name][sum_v] += val
+                if val < name_val[name][min_v]:
+                    name_val[name][min_v] = val
+                elif val > name_val[name][max_v]:
+                    name_val[name][max_v] = val
+    return name_val
+
 if __name__ == '__main__':
 
     time_start = time.time()
-    path_to_file = Path('C://Users/marius.sutkus.QDTEAM/Documents/training_2/measurements_15.txt')
+    path_to_file = Path('C://Users/marius.sutkus.QDTEAM/Documents/training_2/measurements_100.txt')
 
-    name_val = read_file_4(path_to_file=path_to_file)
+    name_val = read_file_7(path_to_file=path_to_file)
     # name_val = cProfile.run('read_file_2(path_to_file=path_to_file)')
 
     read_time = time.time() - time_start
     if name_val:
-        print_data(name_val)
+        print_data_02(name_val)
     # logging_data(name_val)
     # print(name_val)
     time_finish = time.time() - time_start
@@ -158,3 +215,39 @@ if __name__ == '__main__':
 # parsing_time 8.488081932067871
 # printing_time 2.234137773513794
 # [Finished in 12.2s]
+
+# read_file_6, printing_01
+# finish_time:  10.699799537658691
+# parsing_time 5.829745054244995
+# printing_time 4.870054483413696
+
+
+# amount fo cities: 413
+# all val: 15000000
+# avg val per citie: 36319.61259079903
+
+# read_file_7, printing_02
+# finish_time:  12.291387796401978
+# parsing_time 12.286099433898926
+# printing_time 0.005288362503051758
+
+
+# read_file_7, printing_02 100M
+# finish_time:  88.19256091117859
+# parsing_time 88.1845805644989
+# printing_time 0.0079803466796875
+
+# read_file_5, printing 100M
+# finish_time:  118.19615459442139
+# parsing_time 102.68240690231323
+# printing_time 15.513747692108154
+
+# read_file_0, printing 100M
+# finish_time:  74.06772112846375
+# parsing_time 59.81651163101196
+# printing_time 14.251209497451782
+
+# read_file_2, printing 100M
+# finish_time:  75.28374767303467
+# parsing_time 60.41977286338806
+# printing_time 14.863974809646606
--- multiproces_calculate.py
+++ multiproces_calculate.py
@@ -4,39 +4,37 @@ import time
 from pathlib import Path
 
 
+class DataContainer:
+    name_val = {}
+
+
 def avg(par: float):
-    return round(sum(par)/len(par),2)
+    return round(sum(par) / len(par), 2)
 
-def print_data(name_val: dict):
-
-    for name, val in sorted(name_val.items()):
+def print_data(result):
+    for name, val in sorted(result.items()):
         print(f'{name}: {[min(val), avg(val), max(val)]}')
 
-# def pr_data(n: int) -> str:
-#     out = ''
-#     for i in range(n):
-#         out += f'{i=};'
-#     time.sleep(0.001)
-#
-#     return out
-
-
-def run_data(inputs: mp.Queue, result: mp.Queue):
+def run_data(inputs: mp.Queue, outputs: mp.Queue):
     name_val = {}
     while True:
         line = inputs.get()
+        print(line)
+        if line == 'Stop':
+            print('stop run data')
+            # todo hangs when puting data
+            outputs.put(name_val)
+            break
         name, val = line.split(';')
         name_val.setdefault(name, []).append(float(val))
-        # todo
-        result.put(name_val)
+    print('this is outside function')
 
 
 def run_multi_proces(path_file: Path):
-    t_start = time.time()
+    # t_start = time.time()
 
     input_queue = mp.Queue()
     result_queue = mp.Queue()
-    work_queue = mp.Queue()
     process: list = []
 
     nr_pr = 4
@@ -44,24 +42,71 @@ def run_multi_proces(path_file: Path):
         prc = mp.Process(target=run_data, args=[input_queue, result_queue])
         prc.start()
         process.append(prc)
+    t_start = time.time()
+
     with path_file.open() as file:
         for line in file:
             input_queue.put(line)
 
-
-    for prc in process:
-        prc.join()
-    result = []
-    # while not result_queue.empty():
-    res = result_queue.get()
-    print_data(res)
+    # loop time is 27 s.
+    print('end of loop')
     t_end = time.time() - t_start
-    # print(result)
-    # print(t_end)
+    print(t_end)
+
+    for _ in process:
+        print('stop input')
+        input_queue.put('Stop')
+
+    for i, prc in enumerate(process):
+        print(f'{i=}')
+        print('stop process')
+        prc.join()
+
+    result = result_queue.get()
+    print_data(result)
+    t_end = time.time() - t_start
+    print(t_end)
 
 
-if __name__ =="__main__":
-    path_to_file = Path('C://Users/marius.sutkus.QDTEAM/Documents/training_2/measurements_15.txt')
+def run_multi_proces_01(path_file: Path):
+    # t_start = time.time()
+
+    input_queue = mp.Queue()
+    result_queue = mp.Queue()
+    process: list = []
+
+    nr_pr = 4
+    for i in range(nr_pr):
+        prc = mp.Process(target=run_data, args=[input_queue, result_queue])
+        prc.start()
+        process.append(prc)
+    t_start = time.time()
+
+    with path_file.open() as file:
+        for line in file:
+            input_queue.put(line)
+
+    # loop time is 27 s.
+    print('end of loop')
+    t_end = time.time() - t_start
+    print(t_end)
+
+    for _ in process:
+        print('stop input')
+        input_queue.put('Stop')
+
+    for i, prc in enumerate(process):
+        print(f'{i=}')
+        print('stop process')
+        prc.join()
+
+    result = result_queue.get()
+    print_data(result)
+    t_end = time.time() - t_start
+    print(t_end)
+
+
+if __name__ == "__main__":
+    path_to_file = Path('C://Users/marius.sutkus.QDTEAM/Documents/training_2/measurements_2.txt')
     run_multi_proces(path_file=path_to_file)
-
 
